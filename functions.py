005db18fcae6ce0da3492b45b0d253a3d0573fe0
Daniel Bader <dbader@protonmail.com>
2020-07-04 11:31:47 +0200

    initial commit

---
+++ functions.py
@@ -0,0 +1,243 @@
+class NetworkElement:
+
+    ###############################################################
+    # Function that returns the switches' hostname name
+    def swhostname(self, temphost):
+        hostname = ''
+        for row in temphost.split('\n'):
+            hostname = row[9:]
+            break
+        return hostname
+    ###############################################################
+
+    ###############################################################
+    # Function that returns the switches' model name
+    def swmodel(self, tempinv):
+        sw_model = ''
+        sw_line = ''
+        for row in tempinv.split('\n'):
+            if 'PID: ' in row:
+                sw_line = row
+            elif row == '':
+                break
+
+        sw_line = sw_line[5:]
+
+        for char in sw_line:
+            if char == ' ':
+                break
+            sw_model = sw_model + char
+
+        return sw_model
+    ###############################################################
+
+    def __init__(self, index, ip, temphost, tempinv):
+        self.index = index
+        self.hostname = self.swhostname(temphost)
+        self.ip = ip
+        self.model = self.swmodel(tempinv)
+
+    trunks_clean = []
+    trunks_dollar = []
+    access_ports = []
+
+    ###############################################################
+    # Function that returns a list of trunks, with just the abbreviated trunk interface names and a dollar sign
+    def trunkinterfaces(self, temptrunks):
+        trunks = []
+        for row in temptrunks.split('\n'):
+            temp = ''
+            if '0/' not in row:
+                continue
+            for i in row:
+                if i == ' ':
+                    break
+                temp = temp + i
+            trunks.append(temp + '$')
+
+        return trunks
+    ###############################################################
+
+    ###############################################################
+    # Function that returns a list of trunks, with just the abbreviated trunk interface names no dollar sign
+    def trunkinterfacesclean(self, temptrunks):
+        trunks = []
+        for row in temptrunks.split('\n'):
+            temp = ''
+            if '0/' not in row:
+                continue
+            for i in row:
+                if i == ' ':
+                    break
+                temp = temp + i
+            trunks.append(temp)
+
+        return trunks
+
+    ###############################################################
+    # Function that returns a list of trunks, with just the abbreviated trunk interface names and a dollar sign
+    def get_access_ports(self, temp_access_ports, trunks):
+        access_ports = []
+        for row in temp_access_ports.split('\n'):
+            temp = ''
+            if not row:
+                continue
+            for i in row:
+                if i == ' ':
+                    break
+                temp += i
+            if temp in trunks or temp in access_ports:
+                continue
+            access_ports.append(temp)
+
+        return access_ports
+    ###############################################################
+
+
+class UnknownNetworkElement:
+
+    def __init__(self, index, local_swindex, local_swhostname, local_swip, local_swif):
+        self.index = index
+        self.local_swindex = local_swindex
+        self.local_swhostname = local_swhostname
+        self.local_swip = local_swip
+        self.local_swif = local_swif
+
+
+class TrunkLink:
+
+    #HAS-A NetworkElement and a local port x2
+    # Function that looks at trunk interfaces and returns the neighbors ip
+    def __init__(self, index, local_swindex, local_swhostname, local_swip, local_swif):
+        self.index = index
+        self.local_swindex = local_swindex
+        self.local_swhostname = local_swhostname
+        self.local_swip = local_swip
+        self.local_swif = local_swif
+
+    def cdp_neighbors(self, cdpif):
+        neighbor_details = []
+        ip_address = ''
+        for output in cdpif.split('\n'):
+            if 'Device ID:' in output:
+                neighbor_details.append(output[11:])
+                continue
+            if 'IP address:' in output and not ip_address:
+                ip_address = output[14:]
+                neighbor_details.append(ip_address)
+                continue
+            if 'IP Address:' in output and output[14:] == '':
+                return 'ERROR'
+            if 'Total cdp entries displayed : 0' in output:
+                return 'ERROR'
+        if not neighbor_details:
+            return 'ERROR'
+        return neighbor_details
+    #  TODO FIXA SNYGGARE
+
+    def compile_links(self):
+        pass
+    #  take all the link-objects and compile them into true p2p-link objects, that contain's the object of both sides.
+
+    remote_swhostname = ''
+    remote_swip = ''
+
+class SanityChecks:
+    pass
+
+
+class VerifyCommands:
+    # instantiate with skip_switch, command, port_type
+
+    def __init__(self, port_type, command, skip_switch, verification_functions, command_present):
+        self.port_type = port_type
+        self.command = command
+        self.skip_switch = skip_switch
+        self.verification_functions = verification_functions
+        self.command_present = command_present
+
+    found_commands = []
+
+
+    def generate_netmiko_command(self):
+        return self.command, self.port_type
+    # TODO: WANT TO BE ABLE TO SKIP SWITCHES
+
+    def verify_command(self, network_element, interface, result):
+        if self.command_present:
+            if result:
+                self.found_commands.append([network_element, interface])
+                print('command found on: ' + network_element + ': ' + interface)
+        else:
+            if not result:
+                self.found_commands.append([network_element, interface])
+                print('command missing on: ' + network_element + ': ' + interface)
+
+
+    def getitems(macreturn):
+        elements = list(macreturn.split(' '))
+
+        elements2 = list(filter(None, elements))
+
+        return elements2
+
+# Function that returns the appropriately filtered show mac command, for finding a specific host based on mac
+#     def sh_mac_command_host(self):
+#         mac_command_hosts = getitems()
+#         showmaccommand = '{} | exclude CPU'.format(mac_command)
+#
+#         for i in trunkinterfaces():
+#             showmaccommand = '{0}| {1}'.format(showmaccommand, i)
+#
+#         return showmaccommand
+
+    def show_mac_hyphen(self, swmodel):
+        sw = swmodel
+        if sw == 'WS-C2960CX-8PC-L' or sw == 'WS-C2960CX-8PC-L':
+            return 'show mac address-table'
+        else:
+            return 'show mac address-table'
+
+    def sh_mac_command(self, mac_command, trunkinterfaces):
+        showmaccommand = '{} | exclude CPU'.format(mac_command)
+
+        for i in trunkinterfaces:
+            showmaccommand = '{0}| {1}'.format(showmaccommand, i)
+
+        return showmaccommand
+
+    def clean_mac_table(self, macinput):
+        mac_list = []
+        for line in macinput.split('\n'):
+            elements = line.split(' ')
+            elements_list = list(filter(None, elements))
+            try:
+                if elements_list[2] == 'STATIC' or elements_list[2] == 'DYNAMIC':
+                    mac_list.append(elements_list)
+            except IndexError:
+                continue
+        return mac_list
+
+
+
+
+        # take a command
+#
+#     # Function that returns the appropriately filtered show mac command
+#     def sh_mac_command(self):
+#         mac_command = self.show_mac_hyphen()
+#         showmaccommand = '{} | exclude CPU'.format(mac_command)
+#
+#         for i in trunkinterfaces():
+#             showmaccommand = '{0}| {1}'.format(showmaccommand, i)
+#
+#         return showmaccommand
+#
+#     # Function that decides which show mac command will be used
+#     def show_mac_hyphen(self):
+#         sw = swmodel()
+#         if sw == 'WS-C2960CX-8PC-L' or sw == 'WS-C2960CX-8PC-L':
+#             return 'show mac address-table'
+#         else:
+#             return 'show mac address-table'
+
---
+++ main.py
@@ -0,0 +1,249 @@
+from functions import *
+from netmiko import ConnectHandler
+import netmiko
+import getpass
+
+ne_list = []
+link_list = []
+bad_link_list = []
+link_id_list = []
+unknown_ne_list = []
+
+skip_list = []
+not_work = []
+
+
+def get_initial_hosts_csv():
+    csv = open('hosts.csv', 'r')
+
+    delimiter = ','
+    orig_hosts = list(csv.read().split(delimiter))
+
+    hosts = []
+    for i in orig_hosts:
+        hosts.append(i.strip())
+
+    csv.close()
+    return hosts
+
+
+ne_queue = get_initial_hosts_csv()
+
+
+class Engine:
+
+    def __init__(self, user, password):
+        self.username = user
+        self.password = password
+
+
+    def main_loop(self):
+
+        # additional checks
+        # find macs on access-ports
+        # find command in interfaces
+        global b
+        print('Do you want to do additional checks on interface while discovering the network?')
+        additional_checks = input('> ')
+
+        if additional_checks == 'yes':
+            verification_functions = []
+
+            print('Do you want to verify a certain command?')
+            verify_command = input('> ')
+
+            if verify_command == 'yes':
+                verification_functions.append(True)
+            else:
+                verification_functions.append(False)
+
+            print('Find a certain MAC address?')
+            mac_find = input('> ')
+
+            if mac_find == 'yes':
+                verification_functions.append(True)
+            else:
+                verification_functions.append(False)
+
+            print('Check for too many mac addresses on access-ports?')
+            mac_count_find = input('> ')
+
+            if mac_count_find == 'yes':
+                verification_functions.append(True)
+            else:
+                verification_functions.append(False)
+
+            if verification_functions[0]:
+                print('enter to string to search for')
+                command = input('> ')
+
+                print('trunk or access-ports?')
+                while True:
+                    interface_type = input('> ')
+                    if interface_type == 'trunk' or interface_type == 'access':
+                        break
+                    else:
+                        print('You must enter "trunk" or "access"')
+
+                print('Do you want to check if the command is present or missing?')
+                while True:
+                    command_present = input('> ')
+                    if command_present == 'present':
+                        break
+                    elif command_present == 'missing':
+                        command_present = ''
+                        break
+                    else:
+                        print('You must enter either "present" or "missing"')
+
+            if verification_functions[1]:
+                pass
+                # TODO: MAC SEARCH
+
+            if verification_functions[2]:
+                # TODO: specify number if macs and vlans to ignore
+                pass
+                # Get the trunk list from one function and send it to the next:
+
+            b = VerifyCommands(interface_type, command, [], verification_functions, command_present)
+            # TODO: Instantiation doesn't work if verification_functions[0] = False because the variables are never set
+            # but I still need to instantiate if I want to use mac verification
+
+        elif additional_checks == 'no':
+            b = ''
+
+        for val, ip in enumerate(ne_queue):
+
+            if ip in skip_list:
+                ne_list.append('')
+                continue
+            iosv_l2 = {
+                'device_type': 'cisco_ios',
+                'ip': ip,
+                'username': self.username,
+                'password': self.password,
+            }
+            print(f"Currently working on {ip}\n")
+            skip_list.append(ip)
+            try:
+                net_connect = ConnectHandler(**iosv_l2) # TODO: more specific error handling if the connection doesn't work
+            except netmiko.ssh_exception.NetmikoTimeoutException:
+                ne_list.append('')
+                not_work.append(ip)
+                continue
+            except netmiko.ssh_exception.NetmikoAuthenticationException:
+                ne_list.append('')
+                skip_list.append(ip)
+                not_work.append(ip)
+                continue
+            # Below are the commands that get the IOS output that will be used by the various methods, if
+            # that is needed for instantiation
+            swlocal_link_id_list = []
+            temphost = net_connect.send_command('show run | inc hostname')
+            tempinv = net_connect.send_command('show inventory')
+            ne_list.append(NetworkElement(val, ip, temphost, tempinv))
+
+            # More commands needed by functions, after instantiation of NE
+            temptrunks = net_connect.send_command('show interface trunk | begin pruned')
+            temp_access_ports = net_connect.send_command('sh int status | inc connected')
+            ne_list[val].trunks_clean = ne_list[val].trunkinterfacesclean(temptrunks)
+            ne_list[val].trunks_dollar = ne_list[val].trunkinterfaces(temptrunks)
+            ne_list[val].access_ports = ne_list[val].get_access_ports(temp_access_ports, ne_list[val].trunks_clean)
+
+            for val2, item in enumerate(ne_list[val].trunks_clean):
+                link_list.append(TrunkLink(str(val) + '-' + str(val2), ne_list[val].index, ne_list[val].hostname,
+                                           ne_list[val].ip, item))
+                swlocal_link_id_list.append(str(val) + '-' + str(val2))
+
+            for index, i in enumerate(swlocal_link_id_list):
+                cdp_check = net_connect.send_command("sh cdp nei " + str(ne_list[val].trunks_clean[index]) + " detail")
+                try:
+                    link_list[index].remote_swhostname, link_list[index].remote_swip = link_list[index].cdp_neighbors(cdp_check)
+                    if link_list[index].remote_swip not in skip_list:
+                        ne_queue.append(link_list[index].remote_swip)
+                except ValueError:
+                    print('No CDP information!')
+                    bad_link_list.append(TrunkLink(val, ne_list[val].index, ne_list[val].hostname, ne_list[val].ip, str(ne_list[val].trunks_clean[index])))
+                    print(bad_link_list[len(bad_link_list) - 1].local_swip) # TODO: DON'T USE LENGTH AS AN INDEX
+                    print(bad_link_list[len(bad_link_list) - 1].local_swif)
+                except:
+                    print('CDP FUNCTION NOT WORKING')
+                    print(link_list[index].cdp_neighbors(cdp_check))
+                    bad_link_list.append('')
+            link_id_list.append(swlocal_link_id_list)
+
+            if b:
+                if b.verification_functions[0]:
+                    command_list = list(b.generate_netmiko_command())
+                    if command_list[1] == 'trunk':
+                        for trunk in ne_list[val].trunks_clean:
+                            command_check = net_connect.send_command('show run interface ' + trunk + ' | inc ' + command_list[0])
+                            result = command_check
+                            b.verify_command(ne_list[val].ip, trunk, result)
+                    elif command_list[1] == 'access':
+                        for access in ne_list[val].access_ports:
+                            command_check = net_connect.send_command('show run interface ' + access + ' | inc ' + command_list[0])
+                            result = command_check
+                            b.verify_command(ne_list[val].ip, access, result)
+                if b.verification_functions[1]:
+                    pass
+                # Verify if there are too many mac addresses on the access ports
+                if b.verification_functions[2]:
+                    # Get the command that we send to the switch using a number of methods in the VerifyCommands Class
+                    # Also send command to get the show mac output
+                    get_macs = net_connect.send_command(b.sh_mac_command(b.show_mac_hyphen(ne_list[val].model), ne_list[val].trunks_dollar))
+                    #  clean it by removing unwanted lines and return the data is nestled lists:
+                    mac_list = b.clean_mac_table(get_macs)
+                    mac_threshold = 3
+
+                    # find all interfaces
+                    if_count = []
+                    skip_if = []
+                    for line in mac_list:
+                        if_count.append(line[3])
+
+                    for ints, int in enumerate(if_count):
+                        if int in skip_if:
+                            continue
+                        if if_count.count(int) > mac_threshold:
+                            print(f"Too many macs ({if_count.count(int)}) found on {int}")
+                            skip_if.append(int)
+                            unknown_ne_list.append(UnknownNetworkElement(str(val) + str(ints), ne_list[val].index, ne_list[val].hostname, ne_list[val].ip, int))
+
+                net_connect.disconnect()
+
+user_name = input('> ')
+password = getpass.getpass()
+
+a = Engine(user_name, password)
+a.main_loop()
+
+# Code that runs after discovery process, outputs findings
+if not_work:
+    print("switches that are offline or where login doesn't work: ", not_work)
+    print("")
+    print("*" * 10)
+    print("\n")
+for val, item in enumerate(unknown_ne_list):
+    print("Access-ports with too many mac-addresses:")
+    print("Index of element:", unknown_ne_list[val].index)
+    print("Index of local switch:", unknown_ne_list[val].local_swindex)
+    print("Hostname of local switch:", unknown_ne_list[val].local_swhostname)
+    print("IP of local switch:", unknown_ne_list[val].local_swip)
+    print("Local interface:", unknown_ne_list[val].local_swif)
+    print("*" * 10)
+    print("\n")
+try:
+    for val, item in enumerate(ne_list):
+        print("Switches found:")
+        print("Index of element:", ne_list[val].index)
+        print("Hostname of switch:", ne_list[val].hostname)
+        print("IP of switch:", ne_list[val].ip)
+        print("Model:", ne_list[val].model)
+        print("*" * 10)
+        print("\n")
+except:
+    print("No switches found")
+# command was found/missing on:
+# the following trunk interfaces cannot find neighbor interfaces with CDP
+#
